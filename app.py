c09284ce66421c94cce12dd2180e883bc05ae8a9
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2024-11-12 10:50:32 +0100

    add list for orders

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
+orders = []
 
 @app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
 
-@app.route('/pizza-order', methods=["GET"])
+@app.route('/pizza-order', methods=["GET", "POST"])
 def pizza_order():
+    if request.method == "POST":
+        _name = request.form.get("name")
+        _phone = request.form.get("phone")
+        _size = request.form.get("size")
+        order ={
+            "name": _name,
+            "phone": _phone,
+            "size": _size}
+        orders.append(order)
+        print(orders)
     return render_template('pizza_order.html')
 
 
